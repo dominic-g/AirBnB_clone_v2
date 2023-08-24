35ae68b1e9ec5f247a5be31f80f65ab3757c849d
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-24 03:14:16 +0300

    7 file(s)  edited: file.json amenity.cpython-310.pyc base_model.cpyt...

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
+import os
 from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -11,6 +12,7 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
+
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
