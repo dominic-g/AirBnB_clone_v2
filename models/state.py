a6f2c365240ea0d8faf59d3381a3bf8ba6c1b35e
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-09-24 15:45:44 -0400

    edited: state.py Pushed at 2023-09-24 15:45:44

--- models/state.py
+++ models/state.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from models.city import City
-from sqlalchemy.orm import relationship
+"""Defines the State class."""
+
+# Import necessary modules and classes
 from os import getenv
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 import models
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
+    """
+    Represents a state in the HBNB project.
+
+    Attributes:
+        __tablename__ (str): The name of the corresponding database table.
+        name (str): The name of the state (up to 128 characters).
+        cities (relationship): A one-to-many relationship to the City
+        model with a "state" back reference.
+    """
+
+    # Define the table name in the database
+    __tablename__ = "states"
+
+    # Define the 'name' attribute as a column in the table with a
+    # maximum length of 128 characters
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="state",
-                          cascade="delete")
+
+    # Establish a one-to-many relationship with the City model
+    # with a "state" back reference
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """public getter method cities to return the list
-            of City objects from storage linked to the current State
-            """
+            """A relationship between state and city."""
             city_list = []
             all_cities = models.storage.all(City)
             for value in all_cities.values():
